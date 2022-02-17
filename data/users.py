252f5738a949bf1dcc768c2b144480eee9aed5b2
Лесс МА <BearForest2006@gmail.com>
2022-02-17 19:06:40 +0300

    1

--- data/users.py
+++ data/users.py
@@ -8,10 +8,12 @@ class User(SqlAlchemyBase):
     # primary_key указывает на то, что столбец будет ключем, а autoincrement создает id
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     # nullable означает что строечка может ничего не содержать
-    name = sa.Column(sa.Column, nullable=True)
-    about = sa.Column(sa.Column, nullable=True)
+    name = sa.Column(sa.String, nullable=True)
+    about = sa.Column(sa.String, nullable=True)
     # unique проверка на уникальность поля, a index отвечает за то, чтобы быстрее найти эту информацию
     # он добавляет его в буфер, что делает добаление медленне, но поиск быстрее
-    email = sa.Column(sa.Column, nullable=True, unique=True, index=True)
-    name = sa.Column(sa.Column, nullable=True)
+    email = sa.Column(sa.String, nullable=True, unique=True, index=True)
+    heshed_password = sa.Column(sa.String, nullable=True)
+    # default значение по умолчанию
+    create_date = sa.Column(sa.DateTime, default=datetime.now)
 
